9e517d763c473736e5b3720e7716fdc2641db7f7
Anita0815 <anitalau815@gmail.com>
2025-02-02 21:52:17 +0800

    connect to mysql

---
+++ create_database.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE IF NOT EXISTS cinema;
+USE cinema;
+
+-- Create the movies table
+CREATE TABLE IF NOT EXISTS movies (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    genre VARCHAR(255) NOT NULL
+);
+
+-- Insert sample data
+INSERT INTO movies (title, genre) VALUES 
+('Movie 1', 'Action'),
+('Movie 2', 'Comedy');
